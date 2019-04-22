735fee9e144cf17782a3c421440ac998f709186b
Jabjai <jabjai2014@gmail.com>
2019-04-22 21:42:12 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -3,7 +3,8 @@
 require "vendor/autoload.php";
 require_once('vendor/linecorp/line-bot-sdk/line-bot-sdk-tiny/LINEBotTiny.php');
 
-$access_token = '3ALKAbKFoGuJyJnoDdn0HeyfbxLFtEXBKiC0lFeoNl/XbL4WhoCZzefp2n7UDuXaCWfErIDro07BnZNggJmXJChXTIlMPo8LRJ+n1LEgbRUaKehDkiCr5p5CakHrPX+gauOGX/R5bB2e5yi7xjnHDAdB04t89/1O/w1cDnyilFU=';
+$access_token = '3mudBB6jtpesat9HaH/aTyaTu1PkhA4Q0ByMUi6KlFFa3sgTqCXGl/RBHY5BEFSgWKESKam2o3a5MbJdH2aenavKL50EvmTfUEiR51ZPIkQc4Vqz9fdPq+CjZ9zGVRYd+vBCxwbmL8I9j7HPQRzlX5wdB04t89/1O/w1cDnyilFU=
+';
 
 // Get POST body content
 $content = file_get_contents('php://input');
