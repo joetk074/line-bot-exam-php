d806fcf918bc8e37a3ef5dc7d5e42adff9f8e0ce
Jabjai <jabjai2014@gmail.com>
2019-04-23 09:32:53 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -24,7 +24,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' =>  "userId : $userId /r/n roomId : $roomId" 
+				'text' =>  "userId : $userId \r\n roomId : $roomId" 
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
