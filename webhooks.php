aedef8eb9ef849a4bf506a4a5b3555f8903ce13f
Jabjai <jabjai2014@gmail.com>
2019-04-23 09:28:24 +0700

    Update webhooks.php

--- webhooks.php
+++ webhooks.php
@@ -17,14 +17,14 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['source']['roomId'];
-			//$text = 'roomId : ' + $event['source']['roomId'];
+			$text = $text . 'roomId : ' . $event['source']['roomId'];
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => "roomId : $text" 
+				'text' =>  $text 
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
